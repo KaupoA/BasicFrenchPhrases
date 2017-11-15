aac9792bb690a50c9fc2d9bc8632e07ba90b16dc
Kaupo Aun <kaupo.aun@gmail.com>
2017-11-15 22:14:12 +0200

    App done

--- app/src/main/java/com/kast/basicfrenchphrases/MainActivity.java
+++ app/src/main/java/com/kast/basicfrenchphrases/MainActivity.java
@@ -1,13 +1,34 @@
 package com.kast.basicfrenchphrases;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void phraseButton(View view) {
+
+        int id = view.getId();
+        String ourId = "";
+
+        ourId = view.getResources().getResourceEntryName(id);
+
+        int resourceId = getResources().getIdentifier(ourId, "raw", "com.kast.basicfrenchphrases");
+
+        MediaPlayer mplayer = MediaPlayer.create(this, resourceId);
+        mplayer.start();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
